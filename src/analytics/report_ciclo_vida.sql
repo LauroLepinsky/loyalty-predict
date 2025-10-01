f448c7c44b9687142a5228e60ba7443dd61e7f08
Lauro Lepinsky <lauroisaaclepinsky@hotmail.com>
2025-10-01 08:37:18 -0300

    dia 02 projeto

---
+++ src/analytics/report_ciclo_vida.sql
@@ -0,0 +1,9 @@
+SELECT dtRef,
+    descLifeCycle,
+    count(*) AS qtdeCliente
+FROM life_cycle
+WHERE descLifeCycle <> '05-ZUMBI'
+group by dtRef,
+    descLifeCycle
+order by dtRef,
+    descLifeCycle
